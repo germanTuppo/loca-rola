1d81b4d52104c6e8ee41e52f21bed381ed8dc718
German Tuppo <germantuppo@gmail.com>
2021-06-02 11:38:58 -0300

    Mejore menu y algun error de sintaxis

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import './App.css';
 import Navbar from './components/NavBar.js';
 function App() {
   return (
-    <div classNameName="App">
-      <header classNameName="App-header">
-       
-      <Navbar />
+    <div className="App">
+
+      {/* Componente Menú */}
+      <header className="App-header">       
+        <Navbar />
+      </header>
 
 
-        {/* main*/}
+      {/* Aca iria  un componente main*/}
+      <main className="main">
         <h1
-        classNameName ="titulo"
+        className ="titulo"
         >
           Epezando con reactjs
         </h1>
 
         <p
-          classNameName ="parrafo"
+          className ="parrafo"
         >
           Hola mundo!!!
         </p>
 
         <p
-          classNameName ="parrafo"
+          className ="parrafo"
         >
           Para empezar estoy tocando cosas para ver como es el sistema de archivos de react, borre el css que traia por defecto y Eliminé los estilos que traia por defecto.
           Voy a tratar de poner algunos estilos random a ver que onda
@@ -32,14 +35,19 @@ function App() {
         </p>
 
         <a
-          classNameName="linkRepo"
+          className="linkRepo"
           href="https://github.com/germanTuppo/loca-rola.git"
           target="_blank"
           rel="noopener noreferrer"
         >
           Mi repositorio de react
         </a>
-      </header>
+      </main>
+
+
+      {/* Aca va a ir un componente Footer */}
+      
+
     </div>
   );
 }
