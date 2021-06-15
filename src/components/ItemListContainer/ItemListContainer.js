92c118abaa3ed5c798b506601fc19811f8c32326
German Tuppo <germantuppo@gmail.com>
2021-06-15 01:31:50 -0300

    Con los pokemones si, con mi json no

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,20 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import './ItemListContainer.css';
 import ItemList from '../ItemList/ItemList';
-
+import productos from '../../json/productos.json'
 // Item list container es el contenedor de la lista de productos (desafío 3)
 const ItemListContainer = () => {
 
   const [items, setItems] = useState([]);
 
-  // useEffect(() => {
-  //   fetch('../../json/productos.json')
-  //   .then ((response) => setItems (response))
-  //   .then (console.log(items))
+    /*useEffect(() => {
+     fetch(productos)
+      .then (response => response.json())
+      .then (response => setItems(response))
     
-  // }, []);
+   }, []);
 
-  
+  */
+    const [results, setResults] = useState([]);
+
+    useEffect(async () => {
+      fetch("../../json/productos.json")
+        .then(result => {
+          return result.json();
+        })
+        .then(({ results }) => {
+          setResults(results);
+        });
+    }, []);
+
+   
   return (
     <main className="main">
         <h1
@@ -26,9 +39,14 @@ const ItemListContainer = () => {
         <p
           className ="parrafo"
         >
-          Bienvenidos a la  tienda 
+          Bienvenidos a la tienda 
         </p>
          
+        <p>
+          {results.map(element => {
+            return <div>{element.stock}</div>;
+          })}
+        </p>
         <section>
           {/* <ItemList itemsProp={items}/>  */}
         </section>    
